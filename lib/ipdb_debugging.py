37924e1d5e545e95728f1d6fd61d7de0c54a709f
Joseph <joseph.dena@student.moringaschool.com>
2024-01-12 08:25:39 +0300

    Signed-off-by: Joseph <joseph.dena@student.moringaschool.com>

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-
 import ipdb
 
 def plus_two(num):
-    num + 2
+    num = num + 2  # Reassign the result back to num
+    ipdb.set_trace()
     return num
+
+result = plus_two(3)
+print(result)
